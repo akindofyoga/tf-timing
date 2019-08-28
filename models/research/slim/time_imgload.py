cc7b6ecd4ba1f7247bdb8fa5d851f579834dd19a
Roger Iyengar <ri@rogeriyengar.com>
2019-08-27 23:21:12 -0400

    Update time_imgload.py

--- models/research/slim/time_imgload.py
+++ models/research/slim/time_imgload.py
@@ -5,7 +5,6 @@ tf.enable_eager_execution()
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/7/70/EnglishCockerSpaniel_simon.jpg'
 image_string = urllib.urlopen(url).read()
-image = tf.image.decode_jpeg(image_string, channels=3)
 
 start = time.clock()
 image = tf.image.decode_jpeg(image_string, channels=3)
