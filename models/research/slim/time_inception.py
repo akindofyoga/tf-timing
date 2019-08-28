5ee9eca6b3286dfbf45885f080e07ba8fbd06f64
Roger Iyengar <ri@rogeriyengar.com>
2019-08-27 23:20:50 -0400

    added loop for incpetion

--- models/research/slim/time_inception.py
+++ models/research/slim/time_inception.py
@@ -26,26 +26,28 @@ if not tf.gfile.Exists(checkpoints_dir):
 
 image_size = inception.inception_v1.default_image_size
 
-with tf.Graph().as_default():
-    url = 'https://upload.wikimedia.org/wikipedia/commons/7/70/EnglishCockerSpaniel_simon.jpg'
-    image_string = urllib.urlopen(url).read()
-    image = tf.image.decode_jpeg(image_string, channels=3)
-    processed_image = inception_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
-    processed_images  = tf.expand_dims(processed_image, 0)
+url = 'https://upload.wikimedia.org/wikipedia/commons/7/70/EnglishCockerSpaniel_simon.jpg'
+image_string = urllib.urlopen(url).read()
 
-    # Create the model, use the default arg scope to configure the batch norm parameters.
-    with slim.arg_scope(inception.inception_v1_arg_scope()):
-        logits, _ = inception.inception_v1(processed_images, num_classes=1001, is_training=False)
-    probabilities = tf.nn.softmax(logits)
+for i in range(10):
+    with tf.Graph().as_default():
+        image = tf.image.decode_jpeg(image_string, channels=3)
+        processed_image = inception_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
+        processed_images  = tf.expand_dims(processed_image, 0)
 
-    init_fn = slim.assign_from_checkpoint_fn(
-        os.path.join(checkpoints_dir, 'inception_v1.ckpt'),
-        slim.get_model_variables('InceptionV1'))
+        # Create the model, use the default arg scope to configure the batch norm parameters.
+        with slim.arg_scope(inception.inception_v1_arg_scope()):
+            logits, _ = inception.inception_v1(processed_images, num_classes=1001, is_training=False)
+        probabilities = tf.nn.softmax(logits)
 
-    with tf.Session() as sess:
-        init_fn(sess)
+        init_fn = slim.assign_from_checkpoint_fn(
+            os.path.join(checkpoints_dir, 'inception_v1.ckpt'),
+            slim.get_model_variables('InceptionV1'))
 
-        start = time.time()
-        np_image, probabilities = sess.run([image, probabilities])
-        end = time.time()
-        print('Inception time', (end - start))
+        with tf.Session() as sess:
+            init_fn(sess)
+
+            start = time.time()
+            np_image, probabilities = sess.run([image, probabilities])
+            end = time.time()
+            print('Inception time', (end - start))
